08244e33bed13785b83686d14cc3d969f2db4955
Juan Cutino <jcutinocortacero@alumos.ilernasevilla.com>
2023-11-16 11:50:14 +0100

    OffCanvas Color

--- plantilla.php
+++ plantilla.php
@@ -10,7 +10,7 @@
   <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="estilo/style_plantilla.css">
 </head>
 
 <body>
@@ -23,13 +23,22 @@
 
   <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
   <div class="offcanvas-header">
+    <img src="img/Logo_NovaBank_Blanco.png" class="logo" alt="Logotipo">
     <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Colored with scrolling</h5>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
     <p>Try scrolling the rest of the page to see this option in action.</p>
+    <!-- Aquí puedes agregar tus enlaces -->
+    <div class="enlaces">
+      <a href="#">Enlace 1</a>
+      <a href="#">Enlace 2</a>
+      <a href="#">Enlace 3</a>
+      <!-- Agrega más enlaces según sea necesario -->
+    </div>
   </div>
 </div>
+
   </main>
   <footer>
     <!-- place footer here -->
