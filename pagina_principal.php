9ae721ee04b804db319268cfad822ac62c64c596
JCCutino <jcutinocortacero@alumnos.ilernasevilla.com>
2023-12-15 14:22:40 +0100

    Video añadido

--- pagina_principal.php
+++ pagina_principal.php
@@ -93,7 +93,9 @@ include 'funcionalidades/obtenerDatos.php';
         <h1><?php echo "Hola ".$nombreSimple. ", hoy es ".$fecha ?></h1>
         <h1 class="mt-3">IBAN:  <?php echo $IBAN ?></h1>
 
-          
+        <div class="video-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/9sCVcWD1Svs?si=wdAm_-p09q4hscxQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
         </div>
 
         <div class="col-lg-3 col-md-12 sidebar order-1">
